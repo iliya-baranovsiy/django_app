cb53c8b01f1a107d43dea1e457d69177877611a9
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2025-01-28 17:25:14 +0300

    thired com

--- my_test_app/test_app/static/js/redirect.js
+++ my_test_app/test_app/static/js/redirect.js
@@ -1,12 +1,17 @@
 window.onload = function() {
+    initRedirect();
+};
+
+function initRedirect() {
     if (typeof Telegram !== 'undefined') {
         const userId = Telegram.WebApp.initDataUnsafe.user.id;
         console.log('User ID:', userId);
-        let url = "http://localhost:8000/redirect/?user_id="
-        let ans_url = url + userId
-        console.log("new_url= "+ans_url)
-        window.location.href = ans_url;
+        let url = "https://5d88-85-159-229-111.ngrok-free.app/head_page/?user_id=";//url
+        let ans_url = url + userId;
+        setTimeout(function() {
+            window.location.href = ans_url;
+        }, 3000);
     } else {
         console.error('Telegram API не загружен');
     }
-};
+}
